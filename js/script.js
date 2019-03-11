7b3335fec3d10a821dce8828ca62f9a0788493b2
morp <rutesicm27@gmail.com>
2019-03-11 16:19:40 +0100

    Add score tracking

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 const rock = document.querySelector('#rock');
 const paper = document.querySelector('#paper');
 const scissors = document.querySelector('#scissors');
-const playerScore = document.querySelector('player-score');
-const computerScore = document.querySelector('computer-score');
-const roundCount = document.querySelector('round-count');
+const playerScore = document.querySelector('#player-score');
+const computerScore = document.querySelector('#computer-score');
+const roundCount = document.querySelector('#round-count');
 const message = document.querySelector('.message');
 const newGameBtn = document.querySelector('#ng');
+let round = 1;
+let score = [0,0];
 
 
 rock.addEventListener('click', ()=>{playRound('rock',computerPlay())});
@@ -23,35 +25,28 @@ function randNum() {
 }
 
 function playRound(playerChoice,computerChoice) {
-
 	if (playerChoice === computerChoice){
 	message.textContent = `Computer also played ${playerChoice}, TIE! `;		
+	playerScore.textContent = `${score[0]}`;
+	computerScore.textContent = `${score[1]}`;
 }
 	else if(playerChoice == "rock" && computerChoice=="scissors"
      || playerChoice == "paper" && computerChoice=="rock"
      || playerChoice =="scissors" && computerChoice=="paper"){
 		message.textContent = `${playerChoice} beats ${computerChoice} You Win!`;
+		++score[0];
+		playerScore.textContent = `${score[0]}`;
+		computerScore.textContent = `${score[1]}`;
     }
 	else {
 		message.textContent = `${computerChoice} beats ${playerChoice} You Loose!`;
-    }
-  	
+		++score[1];
+		playerScore.textContent = `${score[0]}`;
+		computerScore.textContent = `${score[1]}`
+    }  	
 }
 
-function score(){
-	let winner=playRound();
-	if(winner === "player") ++playerScore;
-	else if(winner === "computer") ++compScore;
-}
 
-function game() {
-	score();
-	if(playerScore === 3)
-    alert("Yeeey, you win!");
-	else if (compScore === 3)
-    alert("Noob, computer owned you!");
-	else game();
-}
 
 function restart(){
 	playerScore = 0;
